9f3eb9a4daaf8597953876f62bb8cec913580435
karukaru <karukaru808@users.noreply.github.com>
2019-05-19 13:13:35 +0900

    [update] エラーメッセージを英語にした

--- Assets/Scripts/NoitomHI5Calibrator.cs
+++ Assets/Scripts/NoitomHI5Calibrator.cs
@@ -123,7 +123,7 @@ namespace HI5
                 {
                     if (!SteamVR.active)
                     {
-                        SetDebugMessage("SteamVRが起動していません！");
+                        SetDebugMessage("Error! Please Start SteamVR!");
                         yield return null;
                         continue;
                     }
@@ -141,7 +141,7 @@ namespace HI5
         {
             if (!HI5_Manager.IsConnected || HI5_Manager.GetGloveStatus().Status != GloveStatus.BothGloveAvailable)
             {
-                SetDebugMessage("Noitom Hi5の接続が確立されていません。");
+                SetDebugMessage("Error! Please Connect Noitom Hi5!");
                 return;
             }
 
